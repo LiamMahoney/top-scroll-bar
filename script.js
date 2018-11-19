03722c5355c89494266bc917d09839589e0a10f7
LiamMahoney <liammahoney96@gmail.com>
2018-11-18 20:04:46 -0600

    basic structure up

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-window.addEventListener('scroll', determineScrollerColors);
+document.body.addEventListener('scroll', determineScrollerColors);
+window.addEventListener('load', determineScrollerColors);
+
 /*
     Calculates the percentages of each color to display for the scroll 
     bar on the top of the screen. 
@@ -6,45 +8,16 @@ window.addEventListener('scroll', determineScrollerColors);
     //TODO: phantom blue on scrollbar.....
 */
 function determineScrollerColors() {
+
     let scrollbar = document.querySelector('.scroller');
-    let current = window.scrollY + window.innerHeight;
+    //users current position on the document
+    let current = document.body.scrollTop + window.innerHeight;
+    //the percent the user is seing of the total page 
     let percentage = (current / document.body.scrollHeight) * 100;
 
     let white = 100 - percentage;
 
-    // Scroller will always be initially yellow, need to 
-    // determine how much yellow to show
-    let yellow = Math.min(percentage, 33.33);
-    if (percentage < 33.33) {
-
     scrollbar.style.background = `linear-gradient(
-        to right, #efeb0b ${yellow}%, white 0
+        to right, black ${percentage}%, white 0
     )`; 
-    }
-
-    // If screen is over 33 percent scrolled, some purple needs
-    // to be shown
-    let purple;
-    if (percentage > 33.33) {
-        // determining how much purple to show
-        purple = Math.min(percentage, 66.66);
-        scrollbar.style.background = `linear-gradient(
-            to right, #efeb0b ${yellow}%, #c610c9 ${purple}%, white 0
-        )`; 
-    } else {
-        purple = 0;
-    }
-
-    // If screen is over 66 percent scrolled, some blue needs 
-    // to be shown
-    let blue;
-    if (percentage > 66.66) {
-        // determining how much blue to show
-        blue = Math.min(percentage, 100);
-        scrollbar.style.background = `linear-gradient(
-            to right, #efeb0b ${yellow}%, #c610c9 ${purple}%, #42e2f4 ${blue}%, white 0
-        )`;  
-    } else {
-        blue = 0;
-    }
 }
